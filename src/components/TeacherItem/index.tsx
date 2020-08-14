51e051f5357b58ff86199120d425f2fa10940e6d
makaifox <yu.trindade@gmail.com>
2020-08-14 09:49:38 -0300

    Commit 1 da versão web

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,56 @@
+import React, { ReactHTML } from 'react';
+
+import whastappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+import api from '../../services/api';
+
+export interface Teacher {
+        avatar :string;
+        bio :string;
+        cost :number;
+        id: number;
+        name :string;
+        subject :string;
+        whatsapp :string;
+    
+};
+
+interface TeacherItemProps {
+    teacher: Teacher; 
+}
+
+
+const  TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher}) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
+    return (
+        <article className="teacher-item">
+                    <header>
+                        <img src={ teacher.avatar} alt={ teacher.name}/>
+                        <div>
+                            <strong>{ teacher.name}</strong>
+                            <span>{ teacher.subject}</span>
+                        </div>
+                    </header>
+
+                    <p>{teacher.bio}</p>
+                    <footer>
+                        <p>
+                            Preço/hora 
+                            <strong>R$ { teacher.cost}</strong>
+                        </p>
+                        <a target="_blank" onClick={createNewConnection} href= {`https://wa.me/${teacher.whatsapp}`}>
+                            <img src={whastappIcon} alt="whatsapp"/>
+                            entrar em contato
+                        </a>
+                    </footer>
+                </article>
+    )
+}
+
+export default TeacherItem;
