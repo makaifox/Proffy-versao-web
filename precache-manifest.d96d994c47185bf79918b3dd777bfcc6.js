b316635b3d0600854adcb610f5d3a6c1fa02ba04
makaifox <yu.trindade@gmail.com>
2021-07-06 10:58:17 -0300

    Updates

---
+++ precache-manifest.d96d994c47185bf79918b3dd777bfcc6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e284a50fa7f8a2eb7018132a964cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6624868d0a6b352d2e9b",
+    "url": "/static/css/main.91964ade.chunk.css"
+  },
+  {
+    "revision": "4e1a47484f951b0919b1",
+    "url": "/static/js/2.1a4f64fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1a4f64fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6624868d0a6b352d2e9b",
+    "url": "/static/js/main.c4338f23.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "1fbb1f8bd147351e311921cdf4e80dc3",
+    "url": "/static/media/back.1fbb1f8b.svg"
+  },
+  {
+    "revision": "9ed76be57e0f41fb4092cbbcb15e0875",
+    "url": "/static/media/give-classes.9ed76be5.svg"
+  },
+  {
+    "revision": "c86272eb642793deb29b7eec180c05a5",
+    "url": "/static/media/landing.c86272eb.svg"
+  },
+  {
+    "revision": "1f9a96a14f86a1ccd682159fe038774d",
+    "url": "/static/media/logo.1f9a96a1.svg"
+  },
+  {
+    "revision": "e042bb237d1a08daabd12bf0a4fb9780",
+    "url": "/static/media/purple-heart.e042bb23.svg"
+  },
+  {
+    "revision": "3c22fb01ba7434dfa52b083a67158019",
+    "url": "/static/media/study.3c22fb01.svg"
+  },
+  {
+    "revision": "744fb7ba0c3df74462352a3858badeb6",
+    "url": "/static/media/warning.744fb7ba.svg"
+  },
+  {
+    "revision": "c2ffa8a6a2f6690a5eba826c625715ab",
+    "url": "/static/media/whatsapp.c2ffa8a6.svg"
+  }
+]);
